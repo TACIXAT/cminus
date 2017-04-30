e6809c9f323f20c058bb65bf6aa0049ef72f3b5f
Douglas Goddard <douglas.gastonguay@gmail.com>
2017-04-30 19:48:37 -0400

    Tabs to spaces.

--- OptPass/CMinusOptPass/CMinusOptPass.cpp
+++ OptPass/CMinusOptPass/CMinusOptPass.cpp
@@ -8,62 +8,62 @@
 using namespace llvm;
 
 namespace {
-	struct CMinusOptPass : public FunctionPass {
-		CMinusOptPass() : FunctionPass(ID) { }
-		static char ID; 
+    struct CMinusOptPass : public FunctionPass {
+        CMinusOptPass() : FunctionPass(ID) { }
+        static char ID; 
 
-		virtual const char *getPassName() const {
-			return "CMinusOptPass";
-		}
+        virtual const char *getPassName() const {
+            return "CMinusOptPass";
+        }
 
-		virtual bool runOnFunction(Function &F) {
-			while(true) {
-				bool entry = true;
-				std::list<BasicBlock *> remove_blocks;
-			
-				for (BasicBlock &B: F) {
-					if(entry) {
-						entry = false;
-						continue;
-					}
-				
-					int pred_count = 0;
-				
-					for(auto it = pred_begin(&B), et = pred_end(&B); it != et; it++) {
-						BasicBlock *pred = *it;
-						pred_count++;
-					} 
-				
-					if(pred_count == 0) {
-						remove_blocks.push_back(&B);
-						
-						for(auto it = succ_begin(&B), et = succ_end(&B); it !=et; it++) {
-							BasicBlock *succ = *it;
-							succ->removePredecessor(&B);
-						} 
-					}
-				}
-			
-				if(remove_blocks.size() == 0)
-					break;
-			
-				for(BasicBlock *B: remove_blocks) {
-					std::cout << "REMOVING: " << std::endl;
-					for(Instruction &I: *B) {
-						I.dump();
-					}
-					B->dropAllReferences();
-					B->removeFromParent();
-				}
-				std::cout << std::endl;
-			}
-			
-			return true;
-		}
-	};
+        virtual bool runOnFunction(Function &F) {
+            while(true) {
+                bool entry = true;
+                std::list<BasicBlock *> remove_blocks;
+            
+                for (BasicBlock &B: F) {
+                    if(entry) {
+                        entry = false;
+                        continue;
+                    }
+                
+                    int pred_count = 0;
+                
+                    for(auto it = pred_begin(&B), et = pred_end(&B); it != et; it++) {
+                        BasicBlock *pred = *it;
+                        pred_count++;
+                    } 
+                
+                    if(pred_count == 0) {
+                        remove_blocks.push_back(&B);
+                        
+                        for(auto it = succ_begin(&B), et = succ_end(&B); it !=et; it++) {
+                            BasicBlock *succ = *it;
+                            succ->removePredecessor(&B);
+                        } 
+                    }
+                }
+            
+                if(remove_blocks.size() == 0)
+                    break;
+            
+                for(BasicBlock *B: remove_blocks) {
+                    std::cout << "REMOVING: " << std::endl;
+                    for(Instruction &I: *B) {
+                        I.dump();
+                    }
+                    B->dropAllReferences();
+                    B->removeFromParent();
+                }
+                std::cout << std::endl;
+            }
+            
+            return true;
+        }
+    };
 }
 
 char CMinusOptPass::ID = 0;
 static RegisterPass<CMinusOptPass> 
-	X("cminusopt", "CMinusOptPass", false, false);
+    X("cminusopt", "CMinusOptPass", false, false);
 
